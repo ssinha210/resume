48039205070955411464b68d59dc39ee54818e69
Sankarshan Sinha <88985473+ssinha210@users.noreply.github.com>
2024-11-12 22:53:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
 
